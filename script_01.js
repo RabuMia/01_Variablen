9ebb3675dba77faecc2ac5f26f5359ab53a74ec3
RabuMia <Excelchen@hotmail.de>
2021-07-28 12:33:50 +0200

    Einführung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,31 @@
 /***** Variablen 01 *******/
 
+//Zeilen-Kommentar
+
+/**
+ * Block-Kommentar
+ * 
+ */
+
+// //01 Ausgabe
+// console.log("Hallo Welt!");
+// console.log(firstName); // nicht deklarierte Var. --> Fehler
+
+// 02 Deklaration + Wertzuweisung I
+
+// let firstName; // Deklaration (Definition)
+// firstName = "Max" // Wertzuweisung
+// console.log(firstName); // Ausgabe
+
+// let familyName = "Müller"; // Deklaration + Wertzuweisung
+// console.log(familyName); // Ausgabe
+// console.log(firstName + " " + familyName); // Ausgabe
+
+// 03 Deklaration + Wertzuweisung II
+
+let firstName,familyName;
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Nachnamen eingeben:");
+console.log(firstName); // Ausgabe
+console.log(familyName); // Ausgabe
+console.log("Hallo " + firstName + " " + familyName + "!" ); // Ausgabe
